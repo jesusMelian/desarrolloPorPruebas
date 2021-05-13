cce90e366188974f2ec1432f4612d757b19624b8
jesuusdavid <jesusdavidmelian@gmail.com>
2021-05-13 12:43:09 +0100

    c

--- src/App.java
+++ src/App.java
@@ -7,4 +7,14 @@ public class App {
     }
 }
 
+class contador{
+    final private int valorIni;
+    final private int incremento;
+    final private int valorMax;
 
+    public contador(int valorIni, int incremento, int valorMax){
+        this.valorIni=valorIni;
+        this.incremento=incremento;
+        this.valorMax=valorMax;
+    }
+}
