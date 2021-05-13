451c31b1d7d7471833f38f22ae488465c3fc48fd
jesuusdavid <jesusdavidmelian@gmail.com>
2021-05-13 12:38:31 +0100

    Pruebo si existe la clase contador(Obviamente no la he creado) - Fallida
    Intento instanciar la clase contador que no existe y me da un error

---
+++ src/pruebaContador.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+public class pruebaContador {
+
+    //COMPROBAR SI EXISTE LA CLASE CONTADOR
+        //ESTO NO FUNCIONA
+        // public void comprobarClaseContador(){
+        //     try{
+        //         Class.forName(contador);
+        //     }catch(ClassNotFoundException e){
+        //         System.out.println("Error no se ha encontrado la clase");
+        //     }
+        // }
+
+        //ESTO SI FUNCIONA
+    
+    contador cont=new contador(1,1,10);
+}
